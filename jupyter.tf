d21ebc71e814e87991300e6a71c5d4de13ad100f
Minrui Gui <guiminrui215@gmail.com>
2024-01-28 01:30:14 -0800

    disable jupyter for now

--- jupyter.tf
+++ jupyter.tf
@@ -1,63 +1,63 @@
-resource "kubernetes_deployment" "jupyter" {
-  metadata {
-    name = "jupyter"
-    labels = {
-      App = "jupyter"
-    }
-  }
+# resource "kubernetes_deployment" "jupyter" {
+#   metadata {
+#     name = "jupyter"
+#     labels = {
+#       App = "jupyter"
+#     }
+#   }
 
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        App = "jupyter"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "jupyter"
-        }
-      }
-      spec {
-        container {
-          image = "jupyter/scipy-notebook"
-          name  = "jupyter-container"
+#   spec {
+#     replicas = 1
+#     selector {
+#       match_labels = {
+#         App = "jupyter"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           App = "jupyter"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "jupyter/scipy-notebook"
+#           name  = "jupyter-container"
 
-          port {
-            container_port = 8888
-          }
+#           port {
+#             container_port = 8888
+#           }
 
-          # Setting the environment variables and args
-        #   env {
-        #     name = "NOTEBOOK_ARGS"
-        #     value = "--ip=0.0.0.0 --NotebookApp.base_url=/jupyter/ --NotebookApp.allow_origin='*'"
-        #   }
-        # env {
-        #     name = "NOTEBOOK_ARGS"
-        #     value = "--ip=0.0.0.0 --NotebookApp.base_url=/jupyter/ --NotebookApp.allow_origin='*'"
-        #   }
-        }
-      }
-    }
-  }
-}
+#           # Setting the environment variables and args
+#         #   env {
+#         #     name = "NOTEBOOK_ARGS"
+#         #     value = "--ip=0.0.0.0 --NotebookApp.base_url=/jupyter/ --NotebookApp.allow_origin='*'"
+#         #   }
+#         # env {
+#         #     name = "NOTEBOOK_ARGS"
+#         #     value = "--ip=0.0.0.0 --NotebookApp.base_url=/jupyter/ --NotebookApp.allow_origin='*'"
+#         #   }
+#         }
+#       }
+#     }
+#   }
+# }
 
 
-resource "kubernetes_service" "jupyter" {
-  metadata {
-    name = "jupyter"
-  }
+# resource "kubernetes_service" "jupyter" {
+#   metadata {
+#     name = "jupyter"
+#   }
 
-  spec {
-    selector = {
-      App = "jupyter"
-    }
-    port {
-      port        = 80
-      target_port = 8888
-    }
+#   spec {
+#     selector = {
+#       App = "jupyter"
+#     }
+#     port {
+#       port        = 80
+#       target_port = 8888
+#     }
 
-    type = "LoadBalancer"
-  }
-}
+#     type = "LoadBalancer"
+#   }
+# }
